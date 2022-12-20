e615ff311a828b5c6c6d36f41aabbe7c140116fc
5hyun <qhslsl@gmail.com>
2022-12-20 22:58:17 +0900

    Feat: 리덕스 설정

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 import Head from "next/head";
 import "antd/dist/antd.css";
 
+import wrapper from "../store/configureStore";
+
 // 모든 페이지에서 공통인 것은 _app.js에 넣고, 특정 컴포넌트끼리 공통인 것은 AppLayout.js에 넣는다.
 // Compoent는 index.js의 리턴 부분, 결국 NodeBird가 부모
 const NodeBird = ({ Component }) => {
@@ -22,4 +24,4 @@ NodeBird.propTypes = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);

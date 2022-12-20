d08b5420029e92d1b4ec6e5941bead1661936aae
5hyun <qhslsl@gmail.com>
2022-12-20 22:57:45 +0900

    Chore: 로그인폼 dispatch 부분 () 빼먹은거 수정

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -1,9 +1,10 @@
-import { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import { Button, Form, Input } from "antd";
 import Link from "next/link";
 import styled from "styled-components";
-import PropTypes from "prop-types";
 import useInput from "../hooks/useInput";
+import { loginAction } from "../reducers/user";
+import { useDispatch } from "react-redux";
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -13,18 +14,15 @@ const FormWrapper = styled(Form)`
   padding: 10px;
 `;
 
-const LoginForm = ({ setIsLoggedIn }) => {
+const LoginForm = () => {
+  const dispatch = useDispatch();
   const [id, onChangeId] = useInput("");
   const [password, onChangePassword] = useInput("");
 
   // 컴포넌트에 props로 넘기는 함수는 useCallback을 써야 최적화가 된다.
-  const onSubmitForm = useCallback(
-    (e) => {
-      console.log(id, password);
-      setIsLoggedIn(true);
-    },
-    [id, password]
-  );
+  const onSubmitForm = useCallback(() => {
+    dispatch(loginAction({ id, password }));
+  }, [id, password]);
 
   return (
     // antDesign의 onFinish는 preventDefault가 자동 적용되어 있다.
@@ -60,10 +58,6 @@ const LoginForm = ({ setIsLoggedIn }) => {
       </ButtonWrapper>
     </FormWrapper>
   );
-};
-
-LoginForm.prototype = {
-  setIsLoggedIn: PropTypes.func.isRequired,
 };
 
 export default LoginForm;

a5ee3fe5fdd49d483b8876319838e3f5a413f4dd
5hyun <qhslsl@gmail.com>
2022-12-28 00:00:40 +0900

    Feat: redux-saga 적용

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,11 +1,23 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 
 import reducer from "../reducers";
+import rootSaga from "../sagas";
+import { lazyEndIndex } from "react-slick/lib/utils/innerSliderUtils";
+
+const loggerMiddleware =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log(action);
+    return next(action);
+  };
 
 const configureStore = () => {
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
@@ -13,6 +25,7 @@ const configureStore = () => {
         //  배포시에도 사용하면 데이터가 다 보여서 보안에 취약하고 history를 다 기록해서 메모리 소모가 심함
         composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 

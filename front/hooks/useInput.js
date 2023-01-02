24544046bab28b2a57064c23778fee6d37b7e3b7
5hyun <qhslsl@gmail.com>
2023-01-03 04:01:28 +0900

    Feat: setState도 사용할 수 있게 수정

--- front/hooks/useInput.js
+++ front/hooks/useInput.js
@@ -1,9 +1,9 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback } from "react";
 
 export default (initValue = null) => {
   const [value, setter] = useState(initValue);
   const handler = useCallback((e) => {
     setter(e.target.value);
   }, []);
-  return [value, handler];
+  return [value, handler, setter];
 };

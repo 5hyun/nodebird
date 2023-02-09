f4e4ba6d57e3210e3247153b44ae5b43a7da4b44
5hyun <qhslsl@gmail.com>
2023-02-10 05:58:44 +0900

    Chore: 게시글 불러오기 연동

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -15,7 +15,6 @@ import {
   ADD_POST_FAILURE,
   ADD_POST_REQUEST,
   ADD_POST_SUCCESS,
-  generateDummyPost,
   LOAD_POSTS_FAILURE,
   LOAD_POSTS_REQUEST,
   LOAD_POSTS_SUCCESS,
@@ -51,16 +50,15 @@ function* addPost(action) {
 }
 
 function loadPostsAPI(data) {
-  return axios.get("/api/post");
+  return axios.get("/posts", data);
 }
 
 function* loadPosts(action) {
   try {
-    // const result = yield call(addPostAPI, action.data);
-    yield delay(1000);
+    const result = yield call(loadPostsAPI, action.data);
     yield put({
       type: LOAD_POSTS_SUCCESS,
-      data: generateDummyPost(10),
+      data: result.data,
     });
   } catch (err) {
     yield put({
@@ -106,6 +104,7 @@ function* addComment(action) {
       data: result.data,
     });
   } catch (err) {
+    console.error(err);
     yield put({
       type: ADD_COMMENT_FAILURE,
       data: err.response.data,

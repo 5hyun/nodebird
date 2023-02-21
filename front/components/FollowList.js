3cb51d32486d9f96f51dafb39600629d8eec7674
5hyun <qhslsl@gmail.com>
2023-02-21 21:28:57 +0900

    Feat: useSWR 설치 및 적용

--- front/components/FollowList.js
+++ front/components/FollowList.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Button, Card, List } from "antd";
-import { StopOutlined } from "@ant-design/icons";
 import PropTypes from "prop-types";
-import { UNFOLLOW_REQUEST, REMOVE_FOLLOWER_REQUEST } from "../reducers/user";
+import { List, Button, Card } from "antd";
+import { StopOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 
-const FollowList = ({ header, data }) => {
+import { UNFOLLOW_REQUEST, REMOVE_FOLLOWER_REQUEST } from "../reducers/user";
+
+const FollowList = ({ header, data, onClickMore, loading }) => {
   const dispatch = useDispatch();
   const onCancel = (id) => () => {
     if (header === "팔로잉") {
@@ -22,19 +23,21 @@ const FollowList = ({ header, data }) => {
 
   return (
     <List
-      style={{ marginBottom: "20px" }}
+      style={{ marginBottom: 20 }}
       grid={{ gutter: 4, xs: 2, md: 3 }}
       size="small"
       header={<div>{header}</div>}
       loadMore={
         <div style={{ textAlign: "center", margin: "10px 0" }}>
-          <Button>더 보기</Button>
+          <Button onClick={onClickMore} loading={loading}>
+            더 보기
+          </Button>
         </div>
       }
       bordered
       dataSource={data}
       renderItem={(item) => (
-        <List.Item style={{ marginTop: "20px" }}>
+        <List.Item style={{ marginTop: 20 }}>
           <Card
             actions={[<StopOutlined key="stop" onClick={onCancel(item.id)} />]}
           >
@@ -49,6 +52,8 @@ const FollowList = ({ header, data }) => {
 FollowList.propTypes = {
   header: PropTypes.string.isRequired,
   data: PropTypes.array.isRequired,
+  onClickMore: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default FollowList;
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,33 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import AppLayout from "../components/AppLayout";
 import Head from "next/head";
 import NicknameEditForm from "../components/NickNameEditForm";
 import FollowList from "../components/FollowList";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
-import {
-  LOAD_FOLLOWERS_REQUEST,
-  LOAD_FOLLOWINGS_REQUEST,
-  LOAD_MY_INFO_REQUEST,
-} from "../reducers/user";
+import { LOAD_MY_INFO_REQUEST } from "../reducers/user";
 import wrapper from "../store/configureStore";
 import axios from "axios";
 import { LOAD_POSTS_REQUEST } from "../reducers/post";
 import { END } from "redux-saga";
+import useSWR from "swr";
+
+const fetcher = (url) =>
+  axios.get(url, { withCredentials: true }).then((result) => result.data);
 
 const Profile = () => {
-  const dispatch = useDispatch();
-
   const { me } = useSelector((state) => state.user);
+  const [followingsLimit, setFollowingsLimit] = useState(3);
+  const [followersLimit, setFollowersLimit] = useState(3);
 
-  useEffect(() => {
-    dispatch({
-      type: LOAD_FOLLOWERS_REQUEST,
-    });
-    dispatch({
-      type: LOAD_FOLLOWINGS_REQUEST,
-    });
-  }, []);
+  const { data: followingsData, error: followingError } = useSWR(
+    `http://localhost:3065/user/followings?limit=${followingsLimit}`,
+    fetcher
+  );
+  const { data: followersData, error: followerError } = useSWR(
+    `http://localhost:3065/user/followers?limit=${followersLimit}`,
+    fetcher
+  );
 
   const router = useRouter();
 
@@ -37,8 +37,21 @@ const Profile = () => {
     }
   }, [me && me.id]);
 
+  const loadMoreFollowers = useCallback(() => {
+    setFollowersLimit((prev) => prev + 3);
+  }, []);
+
+  const loadMoreFollowings = useCallback(() => {
+    setFollowingsLimit((prev) => prev + 3);
+  }, []);
+
   if (!me) {
-    return null;
+    return "내 정보 로딩중...";
+  }
+
+  if (followerError || followingError) {
+    console.error(followerError || followingError);
+    return <div>팔로잉/팔로워 로딩 중 에러가 발생합니다.</div>;
   }
 
   return (
@@ -50,8 +63,18 @@ const Profile = () => {
 
       <AppLayout>
         <NicknameEditForm />
-        <FollowList header="팔로잉" data={me.Followings} />
-        <FollowList header="팔로워" data={me.Followers} />
+        <FollowList
+          header="팔로잉"
+          data={followingsData}
+          onClickMore={loadMoreFollowings}
+          loading={!followingError && !followingsData}
+        />
+        <FollowList
+          header="팔로워"
+          data={followersData}
+          onClickMore={loadMoreFollowers}
+          loading={!followerError && !followersData}
+        />
       </AppLayout>
     </>
   );

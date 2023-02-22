3462cb39429a46145e976c8287c0fa6b31c284a4
5hyun <qhslsl@gmail.com>
2023-02-22 21:11:10 +0900

    Chore: db 연결 수정

--- back/models/index.js
+++ back/models/index.js
@@ -1,25 +1,27 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
+const comment = require('./comment');
+const hashtag = require('./hashtag');
+const image = require('./image');
+const post = require('./post');
+const user = require('./user');
 
-const env = process.env.NODE_ENV || "development";
-const config = require("../config/config")[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
 const db = {};
 
-// node와 Mysql 연결
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-db.Comment = require("./comment")(sequelize, Sequelize);
-db.Hashtag = require("./hashtag")(sequelize, Sequelize);
-db.Image = require("./image")(sequelize, Sequelize);
-db.Post = require("./post")(sequelize, Sequelize);
-db.User = require("./user")(sequelize, Sequelize);
+db.Comment = comment;
+db.Hashtag = hashtag;
+db.Image = image;
+db.Post = post;
+db.User = user;
 
-// 반복문을 돌면서 각각 만들어놨던 associate에서 관계들을 쭉 연결해줌
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(modelName => {
+  db[modelName].init(sequelize);
+});
+
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
--- back/models/user.js
+++ back/models/user.js
@@ -1,44 +1,36 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    "User",
-    {
-      // mysql에는 users 테이블 생성
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class User extends Model {
+  static init(sequelize) {
+    return super.init({
       // id가 기본적으로 들어있다.
       email: {
-        type: DataTypes.STRING(30),
+        type: DataTypes.STRING(30), // STRING, TEXT, BOOLEAN, INTEGER, FLOAT, DATETIME
         allowNull: false, // 필수
+        unique: true, // 고유한 값
       },
       nickname: {
         type: DataTypes.STRING(30),
         allowNull: false, // 필수
-        unique: true, // 고유한 값
       },
       password: {
         type: DataTypes.STRING(100),
         allowNull: false, // 필수
       },
-    },
-    {
-      charset: "utf8",
-      collate: "utf8_general_ci", // 한글 저장
-    }
-  );
-  User.associate = (db) => {
+    }, {
+      modelName: 'User',
+      tableName: 'users',
+      charset: 'utf8',
+      collate: 'utf8_general_ci', // 한글 저장
+      sequelize,
+    });
+  }
+  static associate(db) {
     db.User.hasMany(db.Post);
     db.User.hasMany(db.Comment);
-    db.User.belongsToMany(db.Post, { through: "Like", as: "Liked" });
-    // 나를 팔로우 하는 사람들을 찾는 관계
-    db.User.belongsToMany(db.User, {
-      through: "Follow",
-      as: "Followers",
-      foreignKey: "FollowingId",
-    });
-    // 내가 팔로우하는 사람들을 찾으려면 나를 먼저 찾아야됨
-    db.User.belongsToMany(db.User, {
-      through: "Follow",
-      as: "Followings",
-      foreignKey: "FollowerId",
-    });
-  };
-  return User;
+    db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' })
+    db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followers', foreignKey: 'FollowingId' });
+    db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followings', foreignKey: 'FollowerId' });
+  }
 };
